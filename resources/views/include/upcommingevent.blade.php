a2674ff88e1e741a81e2d6aa5980ace3f3f82039
Kripijenius <18523147@students.uii.ac.id>
2019-12-13 21:46:51 +0700

    apa ini ya allah

--- resources/views/include/upcommingevent.blade.php
+++ resources/views/include/upcommingevent.blade.php
@@ -28,10 +28,89 @@
                </div>
              </div>
            </div>
-         </div>
+          </div>
+        </div>
 
+        <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Event yang Akan Datang
+        <small></small>
+      </h1>
+
+      <!-- Project One -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="https://kurumawisata.com/wp-content/uploads/2019/11/sewa-mobil-jogja-3.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Tour de Merapi</h3>
+          <p>Bersepeda keliling Jogja memang seru. Banyak spot yang terlalu menarik untuk dihiraukan. Ayo, bersepeda dan jelajahi keindahannya.</p>
+          <a class="btn btn-primary" style="background-color:#F38230; border-color:#F38230" href="#">Gabung</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <hr>
+
+      <!-- Project Three -->
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="http://www.jogja.co/wp-content/uploads/2015/10/kraton-jogja-1.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Tour de Merapi</h3>
+          <p>Bersepeda keliling Jogja memang seru. Banyak spot yang terlalu menarik untuk dihiraukan. Ayo, bersepeda dan jelajahi keindahannya.</p>
+          <a class="btn btn-primary" style="background-color:#F38230; border-color:#F38230" href="#">Gabung</a>
+        </div>
+      </div>
+      <!-- /.row -->
+
+
+      <br>
+      <br>
+      <br>
+
+
+         <section class="showcase">
+          <div class="container-fluid p-0">
+            <div class="row no-gutters">
+      
+              <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-4.png');"></div>
+              <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h2>Fully Responsive Design</h2>
+                <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+              </div>
+            </div>
+            <div class="row no-gutters">
+              <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+              <div class="col-lg-6 my-auto showcase-text">
+                <h2>Updated For Bootstrap 4</h2>
+                <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+              </div>
+            </div>
+            <div class="row no-gutters">
+              <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
+              <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h2>Easy to Use &amp; Customize</h2>
+                <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        <br>
+        <br>
+        <br>
          <section class="testimonials text-center bg-light">
             <div class="container">
+              <br>
               <h2 class="mb-5">Tim Oracle</h2>
               <div class="row">
                 <div class="col-lg-3">
