d68420adfd971fb85fb774945a6b66231096c5a7
dilfas <18523054@students.uii.ac.id>
2019-12-16 02:31:56 +0700

    VIRDHA VIRDHA VIRDHAAAAA

--- resources/views/Admin/adminLogin.blade.php
+++ resources/views/Admin/adminLogin.blade.php
@@ -1,7 +1,131 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Admin</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Masuk</h1>
+                  </div>
+
+                  @if ($message = Session::get('alert'))
+                    <div class="alert alert-danger alert-dismissible fade show">
+                      <button type="button" class="close" data-dismiss="alert">×</button> 
+                      <strong>{{ $message }}</strong>
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('admin-login') }}">
+                    @csrf
+
+
+
+                {{-- email --}}
+
+                <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" aria-describedby="emailHelp" placeholder="Email" id="email" {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                      @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                       @endif      
+                    </div>
+
+
+                {{-- password --}}
+                <div class="form-group">
+                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Kata sandi"id="password"{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+
+{{-- 
+                ingat saya --}}
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox small">
+                      <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label class="custom-control-label" for="remember"  >Ingat saya</label>
+                    </div>
+                  </div>
+
+                    {{-- login  --}}
+        
+                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                {{ __('Login') }}
+                            </button>
+                            
+                            @if (Route::has('password.request'))
+                            <a class="text-center small" href="{{ route('password.request') }}">
+                                {{ __('Lupa kata sandi?') }}
+                            </a>
+                        @endif
+    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -79,4 +203,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
